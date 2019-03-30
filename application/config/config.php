bdc9b0ca10dcd3fcefc8a5d0840dcde275fcdcec
David Mazzitelli <davidma1984@gmail.com>
2019-03-29 22:59:14 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://csgoclub.tk';
+$config['base_url'] = 'http://csgolatam.club';
 $config['server_ip_address'] = '54.67.107.185';
 
 /*
